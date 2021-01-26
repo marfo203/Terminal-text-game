058fee8203d67d8054fa4b0ea457d91b2cb4e4b7
Jakob Berggren <jakbe841@student.liu.se>
2021-01-26 18:06:35 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 
 public class Main {
-<<<<<<< HEAD
+
 	private String majs;
-=======
 	
 	    public static void main(String[] args) {
 	        Game g = new Game();
 	        g.run();
 	    }
 	}
->>>>>>> 45f2bcceb8219b0fe2eaf20fbe67defa5eaf5b65
 
