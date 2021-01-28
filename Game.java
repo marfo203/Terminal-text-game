c655010d86c030a7a938090b17e3cd7a3cc6f0c7
Martin Forsberg <marfo203@student.liu.se>
2021-01-28 14:33:27 +0100

    location

--- Game.java
+++ Game.java
@@ -7,28 +7,27 @@ public class Game {
 	private Scanner keyboard;
 	private ArrayList<Location> locations;
 	private Player player;
-	
 
 	public Game() {
-	String name;
-	
-
-		keyboard = new Scanner(System.in);
-		System.out.println("Starting game... \n What is your name?");
+		String name;
+        keyboard = new Scanner(System.in);
+		System.out.println("Welcome to The Puppy of Wall Street \nWhat is your name?");
 		name = keyboard.nextLine();
-		
-		locations = new ArrayList<>();
-		locations.add(new Location("Home", longDesc, shortDesc));
+
+		locations = new ArrayList<Location>();
+		locations.add(new Location("Home", "Du är hemma och mår bra blalalala", "Sydafrika"));
 		locations.add(new Location("WallSt",
 				"Wall steet. The pinnacle of human intellect and  greed. The cradle of innovation. Your second home. Billions of dollars pass through these buildings every day. Every second.\n Your goal "
 						+ name + " is to maximize your profits and become the youngest Puppy.",
 				"Wallnrkoat"));
-		player = new Player(name, locations.get(1));
+		
+		
+		player = new Player(name, locations.get(0));
 	}
 
 	public void run() {
 
-		System.out.println("Welcome to The Puppy of Wall St");
+	
 
 		System.out.println("Hello " + player.getName()
 				+ ", welcome to the high pace world of the stock market! You can move around by typing north/south/west/east. You will have to learn more commands as you play the game! (Hint: there is a command \"help\").");
